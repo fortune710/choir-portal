8fb7052979b9cd2bbb85281a2b40bd452eefe9e6
Fortune Oluwasemilore Alebiosu <fortune0208@yahoo.com>
2025-03-25 14:57:35 -0400

    added session check

--- app/(dashboard-layout)/layout.tsx
+++ app/(dashboard-layout)/layout.tsx
@@ -1,11 +1,18 @@
 import { DashboardLayout } from "@/components/dashboard-layout";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 ;
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const session = await auth();
+
+    if (!session) {
+      //redirect("/login"); // ✅ Redirects unauthenticated users
+    }
     return (
         <div>
             <DashboardLayout>{children}</DashboardLayout>
