fd92237eeba2c3e863b97acc90fdea53af6c309a
raji <kingraj1344@gmail.com>
2025-03-29 18:33:44 +0100

    Enhance searchbar component with debounced search functionality and state management for search input.

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -1,17 +1,52 @@
+'use client'
 import { Filter, Search } from 'lucide-react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useCallback, useEffect, useState } from 'react'
 
 interface SearchbarProps {
     placeholder?: string
 }
 
 export default function Searchbar({ placeholder }: SearchbarProps) {
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const [searchValue, setSearchValue] = useState(searchParams.get('search') ?? '')
+
+    const createQueryString = useCallback(
+        (name: string, value: string) => {
+            const params = new URLSearchParams(searchParams.toString())
+            params.set(name, value)
+            return params.toString()
+        },
+        [searchParams]
+    )
+
+    // Debounce effect
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            router.push(`?${createQueryString('search', searchValue)}`)
+        }, 500) // 500ms delay
+
+        return () => clearTimeout(timer)
+    }, [searchValue, router, createQueryString])
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.target.value)
+    }
+
     return (
         <div className="flex flex-row gap-4 mb-4">
             <div className="relative flex-1">
               <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input type="search" placeholder={placeholder ?? "Search"} className="pl-8" />
+              <Input 
+                type="search" 
+                placeholder={placeholder ?? "Search"} 
+                className="pl-8"
+                onChange={handleSearch}
+                value={searchValue}
+              />
             </div>
             <Button variant="outline" className="md:w-auto w-10">
               <Filter className="mr-2 max-md:mr-0 h-4 w-4" />
